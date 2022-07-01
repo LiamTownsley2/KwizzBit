a6c652042f070d4c3dd3f9d2b6d4bf919e2e34e0
Liam Townsley <contact@liamtownsley.me>
2022-07-01 02:04:21 +0100

    init: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins': ['"Poppins"', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    }
+  ],
+}
