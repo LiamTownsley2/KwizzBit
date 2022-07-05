e09b5382f23cdf964791b887dada24b7fad70096
Liam Townsley <contact@liamtownsley.me>
2022-07-05 03:45:24 +0100

    feat: add more blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,10 @@ module.exports = {
         'orange-1': '#f97316',
         'orange-2': '#f59000',
         'orange-3': '#ec9112',
+        'orange-4': '#fac77b',
         'heading-1': '#292c3e',
         'gray-1': '#f0f1f5',
-        
+        'gray-2': '#d8d8d8',
       },
     },
   },
